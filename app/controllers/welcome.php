eee3cc940a117b9f496fc58f0376c918b89d1b81
Eliasafe <li_okay@hotmail.com>
2017-09-18 15:39:08 -0300

    att

---
+++ app/controllers/welcome.php
@@ -0,0 +1,24 @@
+<?php 
+
+	/**
+	*  Class de Exemplo Controller
+	*/
+
+	use Core\LI_Controller;
+
+	class welcome extends LI_Controller
+	{
+		
+		public function index(){
+			
+			// $this->load->model("exemplo");
+			// $dados["usuario"] = $this->load->exemplo->index();
+
+			$this->load->view("welcome");
+
+		}
+	}
+
+
+
+	?>
